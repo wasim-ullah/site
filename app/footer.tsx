4ba9891aa2744609cc2bd5af647e04e879d5e277
Wasim Ullah <wasim.ullah@aol.com>
2023-07-02 14:03:09 +0300

    Update footer.tsx

--- app/footer.tsx
+++ app/footer.tsx
@@ -4,12 +4,12 @@ export function Footer() {
   return (
     <footer className="p-6 pt-3 pb-6 flex text-xs text-center mt-3 dark:text-gray-400 text-gray-500 font-mono">
       <div className="grow text-left">
-        Marketing Expert
+        
       </div>
       <div>        
         <a target="_blank" href="https://cal.com/wasim/intro"
           className="inline-flex hover:bg-gray-200 dark:hover:bg-[#313131] active:bg-gray-300 dark:active:bg-[#242424] rounded-sm p-2 transition-[background-color]">
-          Lets Talk
+          Meet
         </a>
       </div>
     </footer>
