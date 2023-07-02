afbf467a80b870218484ffc274b4aae5538bf44d
Wasim Ullah <wasim.ullah@aol.com>
2023-07-02 13:32:23 +0300

    Update footer.tsx

--- app/footer.tsx
+++ app/footer.tsx
@@ -2,7 +2,7 @@ import { A } from "./(post)/components/a";
 
 export function Footer() {
   return (
-    <footer className="p-6 pt-3 pb-6 flex text-xs text-center mt-3 dark:text-gray-400 text-gray-500 font-mono">
+    <footer className="p-6 pt-3 pb-6 flex text-xs text-center mt-3 dark:text-gray-400 text-gray-500">
       <div className="grow text-left">
         
       </div>
